d1453ae51c1d93fd9f53b9f03e1779f775033cb0
Arghya <arghyadutta080@gmail.com>
2024-05-14 20:14:30 +0530

    minor fix

--- src/components/home/Goals.tsx
+++ src/components/home/Goals.tsx
@@ -17,7 +17,7 @@ const Goals = () => {
     <div className="mx-2 my-3">
       {/* title */}
       <div className="left-0 right-0 flex flex-row justify-between items-center mt-5 pb-3 mx-2">
-        <span className="text-white text-lg font-medium">${"Today's goal"}</span>
+        <span className="text-white text-lg font-medium">{`${"Today's goal"}`}</span>
         <Image
           src="https://s3-alpha-sig.figma.com/img/210a/8de5/6f295743678d95c92f172e86e2102679?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=lqaScr0cZULLFRy0Rhb10SJBfTyMah78dAgRkLCntQYtTPPbaXppBnjp-6pO2pcozux3hIpAs0Sz1kLhPS-7L97KnHDk0RJDph-OVM-zYJwZmBPyz5Pjb6~RVNjvuQowImy2T-T4GKCWTGaMdZcX06vOysrW6OwWEALnDO7P5NdT~ffeNF6l2Pk9wE-DlNznoDnrovqnTcWp8AFDg8NN--lbW2srl4WxwOjL2I2nGNSp03VAcRJkQuj0uoFQAY4T2qItw3wHiv2XxXT9nrxJynbWuVpocKQs06YSVYliERKCjPnBM5bia6Lh3ACdLj4rVaN8bAmlW7nIWLlKGnSmTQ__"
           alt="_gif"
