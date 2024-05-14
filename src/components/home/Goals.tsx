2f23eb59a4d350d117706a943781a34fb9b433e8
Arghya <arghyadutta080@gmail.com>
2024-05-14 20:10:18 +0530

    fix issues

--- src/components/home/Goals.tsx
+++ src/components/home/Goals.tsx
@@ -4,6 +4,7 @@ import { goals } from "@/utils/constants/goals";
 import React, { useState } from "react";
 import GoalItem from "./GoalItem";
 import { SwipeableButton } from "react-swipeable-button";
+import Image from "next/image";
 
 const Goals = () => {
   const [allState, setAllState] = useState<boolean>(false);
@@ -16,10 +17,12 @@ const Goals = () => {
     <div className="mx-2 my-3">
       {/* title */}
       <div className="left-0 right-0 flex flex-row justify-between items-center mt-5 pb-3 mx-2">
-        <span className="text-white text-lg font-medium">Today's goal</span>
-        <img
+        <span className="text-white text-lg font-medium">${"Today's goal"}</span>
+        <Image
           src="https://s3-alpha-sig.figma.com/img/210a/8de5/6f295743678d95c92f172e86e2102679?Expires=1716768000&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=lqaScr0cZULLFRy0Rhb10SJBfTyMah78dAgRkLCntQYtTPPbaXppBnjp-6pO2pcozux3hIpAs0Sz1kLhPS-7L97KnHDk0RJDph-OVM-zYJwZmBPyz5Pjb6~RVNjvuQowImy2T-T4GKCWTGaMdZcX06vOysrW6OwWEALnDO7P5NdT~ffeNF6l2Pk9wE-DlNznoDnrovqnTcWp8AFDg8NN--lbW2srl4WxwOjL2I2nGNSp03VAcRJkQuj0uoFQAY4T2qItw3wHiv2XxXT9nrxJynbWuVpocKQs06YSVYliERKCjPnBM5bia6Lh3ACdLj4rVaN8bAmlW7nIWLlKGnSmTQ__"
           alt="_gif"
+          height={50}
+          width={54}
           className="h-12 w-14"
         />
       </div>
